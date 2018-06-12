300527e7661d60e7fb6ca32075b1388086714f8b
Marck Manga <Marck@thinkful.com>
2018-06-11 19:27:37 -0700

    Corrections

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
-app.delete('/shopping-list/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
   res.status(204).end();
 });
 
